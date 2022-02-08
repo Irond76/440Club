9d755de6939c8ffadb0278a2035916b90f6e8bf4
Dale Bendixen <dalebendixen@gmail.com>
2022-02-07 19:42:20 -0700

    added jquery and login-signup modals

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('#loginButton').on('click', function() {
+        $('#loginModal').modal('toggle')
+      });
+    $('#signUpButton').on('click', function() {
+        $('#signUpModal').modal('toggle')
+      });
+    
+})
